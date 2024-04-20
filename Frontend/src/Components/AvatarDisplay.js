492578885aa35627e19e0ad1b7b1a7b9130276e1
Keval Bhavsar <kevalbhavsar1404@gmail.com>
2024-04-21 03:07:42 +0530

    fixed biggest error

--- Frontend/src/Components/AvatarDisplay.js
+++ Frontend/src/Components/AvatarDisplay.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Avatar } from './Avatar';
-import { Environment } from '@react-three/drei';
+import { Environment, OrbitControls } from '@react-three/drei';
 
 function AvatarDisplay() {
     return (
-        <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
-            <color attach="background" args={["#ececec"]} />
-            <Avatar />
+        <Canvas shadows camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 0] }} style={{ height: '80vh' }} fallback={null}>
+            {/* <color attach="background" args={["#ececec"]} /> */}
+            <Avatar position={[0, -1.7, -.5]} />
             <Environment preset="sunset" />
         </Canvas>
     );
