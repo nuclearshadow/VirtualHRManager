c74b0b8b1cdb044c64d4bf2a046e30a03270a8a0
Keval Bhavsar <kevalbhavsar1404@gmail.com>
2024-04-20 16:32:31 +0530

    Add Flask API and /send for sending messages to chat

--- Backend/main.py
+++ Backend/main.py
@@ -1,3 +1,4 @@
+from flask import Flask, request
 import google.generativeai as genai
 import os
 
@@ -9,14 +10,28 @@ GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
+
 model = genai.GenerativeModel('gemini-pro')
-# chat = model.start_chat(history=[])
+history = []
 
-# response = chat.send_message("Introduce yourself and welcome the user and ask for introduction from user")
+chat = model.start_chat(history=[])
 
-message = [
-    {'role': 'user', 'parts': ['*You are an HR manager skilled at evaluting employees and asking interview questions, first introduce yourself as a Virtual HR Manager and ask user for introduction*']},
-]
-response = model.generate_content(message)
+def gen_message():
+    # message = [
+    #     {'role': 'user', 'parts': ['*You are an HR manager skilled at evaluting employees and asking interview questions, first introduce yourself as a Virtual HR Manager and ask user for introduction*']},
+    # ]
+    # model.send_message(message)
+    return chat.send_message("*You are an HR manager skilled at evaluting employees and asking interview questions, first introduce yourself as a Virtual HR Manager and ask user for introduction*")
 
-print(response.text)
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello world"
+
+@app.route("/send", methods=['POST'])
+def send():
+    message = request.form['message']
+    message += " *Give feedback on how good of an answer that was and ask the next interview question"
+    return chat.send_message(message).text
+        
