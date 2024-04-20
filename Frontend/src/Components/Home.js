2a4a5572fc47f4468ec9ee8faa9dadebf50b1816
Siddharth Patel <siddharththpatel@gmail.com>
2024-04-20 18:38:15 +0530

    chat ui designed

--- Frontend/src/Components/Home.js
+++ Frontend/src/Components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Register from './Register'
 import Login from './Login'
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import Chat_page from './Chat_page';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Routes>
       <Route path="/" element={<Register/>}></Route>
       <Route  path="/login" element={<Login/>} />
+      <Route  path="/chat" element={<Chat_page/>} />
       </Routes>
       </BrowserRouter>
     </>
