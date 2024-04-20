a1c62fa6155ecba444eaa4ecc510c4b0e6ce1e96
Siddharth Patel <siddharththpatel@gmail.com>
2024-04-21 01:16:40 +0530

    added speech recognition and changed layout

--- Frontend/src/Components/Header.js
+++ Frontend/src/Components/Header.js
@@ -4,12 +4,12 @@ import '../Components/Header.css'
 export default function Header() {
     return (
         <>
-            <header class="site-header">
-                <div class="site-identity">
+            <header className="site-header">
+                <div className="site-identity">
                     <h1><a href="#">Virtual HR Manager</a></h1>
                 </div>
-                <nav class="site-navigation">
-                    <ul class="nav">
+                <nav className="site-navigation">
+                    <ul className="nav">
                         <li><a href="#">About</a></li>
                         <li><a href="#">News</a></li>
                         <li><a href="#">Contact</a></li>
