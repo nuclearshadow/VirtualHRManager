114244d1ebee79c209a56750750e78bfd9db89d8
Keval Bhavsar <kevalbhavsar1404@gmail.com>
2024-04-20 16:57:42 +0530

    Update api

--- Backend/main.py
+++ Backend/main.py
@@ -10,28 +10,21 @@ GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
-
 model = genai.GenerativeModel('gemini-pro')
-history = []
 
 chat = model.start_chat(history=[])
 
-def gen_message():
-    # message = [
-    #     {'role': 'user', 'parts': ['*You are an HR manager skilled at evaluting employees and asking interview questions, first introduce yourself as a Virtual HR Manager and ask user for introduction*']},
-    # ]
-    # model.send_message(message)
-    return chat.send_message("*You are an HR manager skilled at evaluting employees and asking interview questions, first introduce yourself as a Virtual HR Manager and ask user for introduction*")
-
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello world"
+chat_name = "Jonathan"
+init_message = f"*You are an HR manager skilled at evaluting employees and asking interview questions, first introduce yourself in a short way as a Virtual HR Manager with just the name {chat_name} only, you are taking interview of the user applying for a job, ask user for introduction.*"
 
-@app.route("/send", methods=['POST'])
-def send():
+@app.route("/chat/get-init-message", methods=['GET'])
+def chat_get_init_message():
+    return chat.send_message(init_message).text
+    
+@app.route("/chat/send-message", methods=['POST'])
+def chat_send_message():
     message = request.form['message']
     message += " *Give feedback on how good of an answer that was and ask the next interview question"
     return chat.send_message(message).text
-        
