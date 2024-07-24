f4600f56208663eec37d5bb9ca37f8b590045e7d
Siddharth Patel <siddharththpatel@gmail.com>
2024-07-24 11:52:08 +0530

    AvatarDisplay.js Normal Changes

--- Frontend/src/Components/AvatarDisplay.js
+++ Frontend/src/Components/AvatarDisplay.js
@@ -23,7 +23,7 @@ import { Environment, OrbitControls } from '@react-three/drei';
 
 function AvatarDisplay({ isTalking }) {
     return (
-        // AVATAR 
+        // Avatar Display 
         <Canvas shadows camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 0] }} style={{ height: '100%' }} fallback={null}>
             {/* <color attach="background" args={["#ececec"]} /> */}
             <Avatar isTalking={isTalking} position={[0, -1.7, -.5]} />
